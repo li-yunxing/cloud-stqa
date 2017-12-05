334d10ad4f87400f2753bb8448d3602896024692
li-yunxing <li_yunxing@nhn-st.com>
2017-12-05 16:39:53 +0800

    update for 12/5

--- apigate/api_server/CloudSTQA/routes/testPageSleep.js
+++ apigate/api_server/CloudSTQA/routes/testPageSleep.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('access/testPage', { title: 'Cloud_STQA' });
-//});
-
-
 router.get('/', function(req, res, next) {
   var fiveSecond = 1000 * 5;
   setTimeout(function() {
-	  res.render('access/testPage', { title: 'Cloud_STQA' });
+	  res.render('access/testPageSleep', { title: 'Cloud_STQA' });
   }, fiveSecond);
 });
 
