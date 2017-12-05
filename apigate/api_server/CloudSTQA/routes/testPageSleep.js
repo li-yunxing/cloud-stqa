2d36d9dbca861eaa2acc8074fe2020923a6fd2de
li-yunxing <li_yunxing@nhn-st.com>
2017-12-05 17:14:37 +0800

    Update for 12/5

--- apigate/api_server/CloudSTQA/routes/testPageSleep.js
+++ apigate/api_server/CloudSTQA/routes/testPageSleep.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+//var sleep = require('sleep-async')();
 
 router.get('/', function(req, res, next) {
   var fiveSecond = 1000 * 5;
   setTimeout(function() {
 	  res.render('access/testPageSleep', { title: 'Cloud_STQA' });
   }, fiveSecond);
+  
 });
 
 module.exports = router;
