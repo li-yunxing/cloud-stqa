48a9c4675e788bfdb6e111b3ca76bf8cfaf5a0da
li-yunxing <li_yunxing@nhn-st.com>
2017-12-08 10:34:57 +0800

    update 12/8

--- apigate/api_server/CloudSTQA/routes/client_info.js
+++ apigate/api_server/CloudSTQA/routes/client_info.js
@@ -6,13 +6,17 @@ router.get('/', function(req, res, next) {
 	//Obtain client header info
 	console.log(req.headers);
 	
-	//send client IP Address
+	//send remote IP Address
 	const ip = res.socket.remoteAddress;
 	const port = res.socket.remotePort;
 	console.log(ip);
 	console.log(port);
 	res.end('Your IP Adreess is '+ ip + ' - ' + 'your port is ' + port);
-	  
+	
+	//send x-forwarded IP Address
+	var client_IP = req.header('x-forwarded-for');
+	console.log(client_IP);
+
 	});
 
 module.exports = router;
