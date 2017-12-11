9c93c15a007b3f20e962435c701cb9342cbf3f39
li-yunxing <li_yunxing@nhn-st.com>
2017-12-11 15:44:39 +0800

    update 12/11

--- apigate/api_server/CloudSTQA/routes/MockResponse.js
+++ apigate/api_server/CloudSTQA/routes/MockResponse.js
@@ -6,12 +6,14 @@ router.use('/', function(req, res, next) {
 	if(req.method == 'GET'){
 
 		res.setHeader('Content-Type', 'cloud/stqa;charset=UTF-8');
-		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8');	
+		res.setHeader('Content-Language', 'cn');
+		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8; Content-Language with cn');	
 		
 	}else if(req.method == 'POST'){
 		
 		res.setHeader('Content-Type', 'cloud/stqa;charset=UTF-8');
-		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8');
+		res.setHeader('Content-Language', 'cn');
+		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8; Content-Language with cn');
 	}
 });
 
--- apigate/api_server/CloudSTQA/routes/ModifyHeaders.js
+++ apigate/api_server/CloudSTQA/routes/ModifyHeaders.js
@@ -6,16 +6,22 @@ router.use('/', function(req, res, next) {
 	if(req.method == 'GET'){
 
 		var Accept_Language = req.header('Accept-Language');
+		var From = req.header('From');
 		console.log(Accept_Language);
+		console.log(From);
 		res.setHeader('Content-Type', 'cloud/stqa;charset=UTF-8');
-		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8');	
+		res.setHeader('Content-Language', 'cn');
+		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8; Content-Language with cn');	
 		
 	}else if(req.method == 'POST'){
 		
 		var Accept_Language = req.header('Accept-Language');
+		var From = req.header('From');
 		console.log(Accept_Language);
+		console.log(From);
 		res.setHeader('Content-Type', 'cloud/stqa;charset=UTF-8');
-		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8');	
+		res.setHeader('Content-Language', 'cn');
+		res.end('Server response header Content-Type with cloud/stqa;charset=UTF-8; Content-Language with cn');	
 	}
 });
 
