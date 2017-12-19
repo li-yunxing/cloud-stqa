bf1414ab2fa8fa13e11906e84594f9356956d439
li-yunxing <li_yunxing@nhn-st.com>
2017-12-19 10:42:57 +0800

    update 12/19

--- apigate/api_server/CloudSTQA/routes/cookie_info.js
+++ apigate/api_server/CloudSTQA/routes/cookie_info.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
 	  //set cookie info
 	  res.setHeader('Set-Cookie', ['type=cloudstqa', 'language=javascript']);
 	  
+	  //임시 추가
+	  res.setHeader('x-toast-cloud-stqa', ['cloudstqa']);
+	  res.setHeader('anyheaders', ['any']);
+	  
 	  //output cookie info
 	  res.send('Request cookie info from client: ' + cookie);
 	  
