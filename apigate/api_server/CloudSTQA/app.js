b06df34c9cb7bd7e7849fa11544278d9adbc722d
li-yunxing <li_yunxing@nhn-st.com>
2017-12-22 15:53:26 +0800

    update 12/22

--- apigate/api_server/CloudSTQA/app.js
+++ apigate/api_server/CloudSTQA/app.js
@@ -56,7 +56,7 @@ app.use('/access/testPage/sleep', testPageSleep);
 app.use('/access/testPage/timeOut', testPageTimeOut);
 
 //Obtain Parameter with GET/POST method 
-app.use('/access/{param}', param);
+app.use('/access/%7Bparam%7D', param);
 
 //Test Send cookie
 app.use('/access/cookie', cookie_info);
