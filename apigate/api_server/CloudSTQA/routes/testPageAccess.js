2c56a5489b44f595772a235b89aa0af17158e33e
li-yunxing <li_yunxing@nhn-st.com>
2017-12-26 18:24:33 +0800

    update 12/26

--- apigate/api_server/CloudSTQA/routes/testPageAccess.js
+++ apigate/api_server/CloudSTQA/routes/testPageAccess.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var http = require("http");
 
 /* GET home page. */
 router.use('/', function(req, res, next) {
@@ -22,6 +23,41 @@ router.use('/', function(req, res, next) {
         res.render('access/testPage', { title: 'Cloud_STQA' });
         break;
   }
+  
+
+//post log to log & crash
+  var options = {
+    "method": "POST",
+    "hostname": "api-logncrash.cloud.toast.com",
+    "port": null,
+    "path": "/v2/log",
+    "headers": {
+      "content-type": "application/json",
+      "cache-control": "no-cache",
+    }
+  };
+
+  var req = http.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+
+  req.write(JSON.stringify({ projectName: 'bN939DicBNPHrUUp',
+    projectVersion: '1.0.0',
+    logVersion: 'v2',
+    body: 'This log message come from HTTP server.',
+    logSource: 'http',
+    logType: 'nelo2-log',
+    host: '133.186.138.136' }));
+  req.end();
 
 });
 
