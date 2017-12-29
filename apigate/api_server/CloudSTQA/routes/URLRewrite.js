ee3fb9e1d02e5a97baea581279daf02e3f4114d4
li-yunxing <li_yunxing@nhn-st.com>
2017-12-29 15:48:19 +0800

    update 12/29

--- apigate/api_server/CloudSTQA/routes/URLRewrite.js
+++ apigate/api_server/CloudSTQA/routes/URLRewrite.js
@@ -7,20 +7,7 @@ router.use('/', function(req, res, next) {
   switch (req.method) { 
 
     case 'GET':
-        
-    	//---------------Modify Headers와 URL Rewrite 동시 적용 시 Request Headers 값 체크--------------
-    	var Accept_Language = req.header('Accept-Language');
-
-        if(Accept_Language == 'apigw_request'){
-        	
-        	console.log(Accept_Language);
-        	res.end('Server recieved header info - Accept-Language: ' + Accept_Language);	
-        	
-        }else{
-
-        	res.render('access/result', { title: 'Cloud_STQA' });      	
-        }
-        //-------------------------------------------------------------------------------------
+        res.render('access/result', { title: 'Cloud_STQA' });      	
         break;
     
     case 'POST':
